ebc68b8a1c6f0d5af3fd730f51be374dfcb7439e
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-02-28 17:26:07 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     About: ['doc1'],
     Prerequisites: ['doc2'],
-    Voiceflow: ['doc4','doc5','doc6'],
+    Voiceflow: ['doc4', 'doc5', 'doc6', 'doc7'],
   },
 };
